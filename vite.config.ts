f0e6f01252a9093280f9256b30619f21b1f71b9a
dennis <gicbosses@gmail.com>
2026-02-08 06:45:52 -0400

    build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets load correctly relative to the plugin folder
+  base: '/Cricketcore/', // Ensures assets load correctly relative to the repository name
   build: {
     rollupOptions: {
       output: {
